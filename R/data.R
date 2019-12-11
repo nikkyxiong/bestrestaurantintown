dbb0f8b9e4a81aa62dd5b3909cfde220e0978cd4
nikkyxiong <nikkyxiong97@gmail.com>
2019-12-11 10:15:39 -0500

    v_0.1.1

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Information about Counties in California
 #'
 #' This data provides a list of counties in California
-#' with information of name, population in 2010 ans 2019, and land area.
+#' with information of name, population in 2010 and 2019, and land area.
 #'
 #' @format The ca data frame contains 55 observations on 4 variables.
 #' \describe{
@@ -11,11 +11,12 @@
 #'   \item{Area(km^2)}{Area in Sqaured Kilometers}
 #'}
 #' @source \url{https://en.wikipedia.org/wiki/List_of_cities_and_towns_in_California}
+#' @source \url{http://worldpopulationreview.com/states/california-population/cities/}
 "ca"
 
 
 
-#' Average Heights and Weights for American Women
+#' Information about Best-Rated Restaurants in Counties of California and More
 #'
 #' This data contains both information from ca and pp19 and
 #' information from Yelp API, such as the number of restaurants, average rating,
